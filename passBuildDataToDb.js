9790f8f7809e86b3f458c3e9cf9392acb737e153
VaibhavPawar212303 <pawarvaibhav.vppv@gmail.com>
2023-10-31 08:44:28 +0530

    update the yml and build push status

---
+++ passBuildDataToDb.js
@@ -0,0 +1,26 @@
+const testFolder = "./newman/";
+const fs = require("fs");
+const url = "http://localhost:8000/api/build/createbuild";
+fs.readdir(testFolder, (err, files) => {
+  console.log(files);
+  fs.readFile(
+    `./newman/${files[0]}`,
+    { encoding: "utf-8" },
+    function (err, data) {
+      let collectionTestData = JSON.parse(data);
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          project_id: 2,
+          project_type: "Postman_API Test",
+          BuildData: collectionTestData,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+  );
+});
